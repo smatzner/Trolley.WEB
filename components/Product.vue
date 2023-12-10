d33e87a20f720673e4345c7e291d7c3e056f9505
Stefan Matzner <stefanmatzner@gmx.at>
2023-12-10 16:11:06 +0100

    added contextmenu to products to change products characteristics on the fly

--- components/Product.vue
+++ components/Product.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+
+import {algorithms} from "iron-webcrypto";
+
 const props = defineProps({
   product: {
     required: true,
@@ -7,17 +10,63 @@ const props = defineProps({
 })
 
 const productStore = useProductStore()
+const menu = ref()
+const items = ref([
+  {
+    label: 'Löschen',
+    command: () => removeProductFromShoppingList()
+  },
+  {
+    label: 'Anzahl',
+    command: () => {
+      productStore.updateProductFromShoppingList(props.product)
+    }
+  },
+  {
+    label: 'Bio',
+    command: () => {
+      props.product.isOrganic = !props.product.isOrganic
+    }
+  },
+  {
+    label: 'Diskonter',
+    command: () => {
+      props.product.isDiscountProduct = !props.product.isDiscountProduct
+      productStore.updateProductFromShoppingList(props.product)
+    }
+  }
+])
 
-function removeProductFromShoppingList(){
+// watch(menu => console.log(menu))
+
+function onRightClick(event: any) {
+  console.log(event)
+  menu.value.show(event)
+}
+
+function removeProductFromShoppingList() {
   productStore.removeProductFromShoppingList(props.product.productId)
 }
 </script>
 
 <template>
-  <PrimeButton class="bg-trolley-primary flex-col text-white aspect-square" @click="removeProductFromShoppingList()">
+  <PrimeButton class="bg-trolley-primary flex-col text-white aspect-square" @click="removeProductFromShoppingList()"
+               @contextmenu="onRightClick">
     <Icon :name="'fa6-solid:'+ product.iconName" class="text-7xl"/>
-    <span class="text-xs mt-1">{{ product.productName }}</span>
+    <span class="text-xs mt-1" v-if="product.isOrganic">Bio {{ product.productName }}</span>
+    <span class="text-xs mt-1" v-else-if="product.isDiscountProduct">Diskonter {{ product.productName }}</span>
+    <span class="text-xs mt-1" v-else>{{ product.productName }}</span>
     <span class="text-xs mt-1">{{ product.amount }}</span>
+    <PrimeContextMenu ref="menu" :model="items">
+            <template #item="{item, props}">
+              <PrimeInputNumber v-if="item.label === 'Anzahl'" v-model="product.amount" v-bind="props.action" inputId="horizontal-buttons" showButtons buttonLayout="horizontal" :min="1" class="ms-2 me-12" inputClass="text-sm text-center w-10 h-8"
+                                decrementButtonClass="w-1/3 h-8 bg-trolley-primary  border-trolley-primary" incrementButtonClass="w-1/3 h-8 bg-trolley-primary border-trolley-primary">
+                <template #decrementbuttonicon>-</template>
+                <template #incrementbuttonicon>+</template>
+              </PrimeInputNumber>
+              <span v-else class="mx-4 my-3" v-bind="props.action">{{item.label}}</span>
+            </template>
+    </PrimeContextMenu>
   </PrimeButton>
 </template>
 
--- stores/ProductStore.js
+++ stores/ProductStore.js
@@ -1,109 +1,140 @@
 export const useProductStore = defineStore('product', () => {
-    const products = useState('products', () => [])
-    const productsInShoppingList = useState('productsInShoppingList', () => [])
-    const categories = useState('categories', () => [])
-    const shoppingList = useState('shoppingList', () => [])
-    const costsPerMarket = useState('costsPerMarket', () => [
-        {"marketName": "Billa", "totalPrice": 0},
-        {"marketName": "Spar", "totalPrice": 0},
-        {"marketName": "Hofer", "totalPrice": 0}
-    ])
+        const products = useState('products', () => [])
+        const productsInShoppingList = useState('productsInShoppingList', () => [])
+        const categories = useState('categories', () => [])
+        const shoppingList = useState('shoppingList', () => [])
+        const costsPerMarket = useState('costsPerMarket', () => [
+            {"marketName": "Billa", "totalPrice": 0},
+            {"marketName": "Spar", "totalPrice": 0},
+            {"marketName": "Hofer", "totalPrice": 0}
+        ])
 
-    async function loadProducts() {
-        await new Promise((resolve, reject) => {
-            useFetch('https://localhost:7124/api/product', {
-                onResponse({response}) {
-                    products.value = response._data
-                    resolve()
-                },
-                onError(error) {
-                    reject(error)
-                }
+        async function loadProducts() {
+            await new Promise((resolve, reject) => {
+                useFetch('https://localhost:7124/api/product', {
+                    onResponse({response}) {
+                        products.value = response._data
+                        resolve()
+                    },
+                    onError(error) {
+                        reject(error)
+                    }
+                })
             })
-        })
 
-        if (JSON.parse(localStorage.getItem('shoppingList'))) {
-            shoppingList.value = JSON.parse(localStorage.getItem('shoppingList'))
-            shoppingList.value.forEach(productInShoppingList => {
-                console.log(productInShoppingList.amount)
-                const matchingProduct = products.value.find(product => product.productId === productInShoppingList.productId ?? product)
-                productsInShoppingList.value.push({...matchingProduct, amount: productInShoppingList.amount})
-            })
+            if (JSON.parse(localStorage.getItem('shoppingList'))) {
+                shoppingList.value = JSON.parse(localStorage.getItem('shoppingList'))
+                shoppingList.value.forEach(productInShoppingList => {
+                    const matchingProduct = products.value.find(product => product.productId === productInShoppingList.productId ?? product)
+                    productsInShoppingList.value.push({...matchingProduct, amount: productInShoppingList.amount})
+                })
+                getCategories()
+                await AddProductsAndCalculateList(shoppingList.value)
+            }
+        }
+
+        async function addProductToShoppingList(shoppingListProduct) {
+            try {
+                await new Promise((resolve) => {
+                    useFetch('https://localhost:7124/api/product/search?name=' + shoppingListProduct.productName, {
+                        onResponse({response}) {
+                            const fetchedProduct = response._data.find(fetchedProduct => {
+                                return (
+                                    fetchedProduct.isOrganic === shoppingListProduct.isOrganic &&
+                                    fetchedProduct.isDiscountProduct === shoppingListProduct.isDiscountProduct
+                                )
+                            })
+                            if (productsInShoppingList.value.find(product => product.productId === fetchedProduct.productId)) {
+                                const productIndex = productsInShoppingList.value.findIndex(product => product.productId === fetchedProduct.productId)
+                                productsInShoppingList.value[productIndex].amount += shoppingListProduct.amount
+                                shoppingList.value[productIndex].amount += shoppingListProduct.amount
+                            } else {
+                                productsInShoppingList.value.push({...fetchedProduct, amount: shoppingListProduct.amount})
+                                shoppingList.value.push({
+                                    productId: fetchedProduct.productId,
+                                    amount: shoppingListProduct.amount
+                                })
+                            }
+                            localStorage.setItem('shoppingList', JSON.stringify(shoppingList.value))
+                            resolve()
+                        }
+                    })
+                })
+                // const {data} = await useFetch('/api/product/suche?name=' + productName,{
+                //         transform: (fetchedProducts) => {
+                //             return fetchedProducts.filter((fetchedProduct, index, self) => {
+                //                 return index === self.findIndex((product) => (product.productName === fetchedProduct.productName))
+                //             })
+                //         },
+                //         onResponse({response}) {
+                //             /// aufruf shoppinglist controller?
+                //             // console.log(response._data)
+                //         }
+                // })
+                // const data = null
+                // console.log(data)
+
+                await AddProductsAndCalculateList(shoppingList.value)
+
+                getCategories()
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        async function updateProductFromShoppingList(updatedProduct){
+            const productIndex = productsInShoppingList.value.findIndex(product => product.productId === updatedProduct.productId)
+
+            console.log(updatedProduct)
+
+            productsInShoppingList.value.splice(productIndex, 1)
+            shoppingList.value.splice(productIndex, 1)
+
+            await addProductToShoppingList(updatedProduct)
+
+        }
+
+        async function removeProductFromShoppingList(productId) {
+            const productIndex = productsInShoppingList.value.findIndex(product => product.productId === productId)
+
+            productsInShoppingList.value.splice(productIndex, 1)
+            shoppingList.value.splice(productIndex, 1)
+            localStorage.setItem('shoppingList', JSON.stringify(shoppingList.value))
+
             getCategories()
             await AddProductsAndCalculateList(shoppingList.value)
         }
+
+        function getCategories() {
+            const categoriesMap = new Map()
+
+            productsInShoppingList.value.forEach(product => {
+                categoriesMap.set(product.productCategoryId, product.productCategoryName)
+            })
+
+            categories.value = Array.from(categoriesMap.keys())
+                .sort((a, b) => a - b)
+                .map(categoryId => categoriesMap.get(categoryId));
+        }
+
+        async function AddProductsAndCalculateList(products) {
+            const {data} = await useFetch('https://localhost:7124/api/TemporaryShoppingList/AddProductAndCalculateList', {
+                method: 'POST',
+                body: JSON.stringify(products),
+            })
+            costsPerMarket.value = data
+        }
+
+        return {
+            products,
+            productsInShoppingList,
+            categories,
+            costsPerMarket,
+            shoppingList,
+            loadProducts,
+            addProductToShoppingList,
+            updateProductFromShoppingList,
+            removeProductFromShoppingList
+        }
     }
-
-    async function addProductToShoppingList(productName, productAmount) {
-        const {data} = await useFetch('https://localhost:7124/api/product/search?name=' + productName, {
-            transform: (fetchedProducts) => {
-                return fetchedProducts.filter((fetchedProduct, index, self) => {
-                    return index === self.findIndex((product) => (product.productName === fetchedProduct.productName))
-                })
-            }
-        })
-        // const {data} = await useFetch('/api/product/suche?name=' + productName,{
-        //         transform: (fetchedProducts) => {
-        //             return fetchedProducts.filter((fetchedProduct, index, self) => {
-        //                 return index === self.findIndex((product) => (product.productName === fetchedProduct.productName))
-        //             })
-        //         },
-        //         onResponse({response}) {
-        //             /// aufruf shoppinglist controller?
-        //             // console.log(response._data)
-        //         }
-        // })
-        console.log(data.value[0])
-        productsInShoppingList.value.push({...data.value[0],amount: productAmount})
-        console.log(productsInShoppingList.value)
-
-        shoppingList.value.push({productId: data.value[0].productId, amount: productAmount})
-        localStorage.setItem('shoppingList', JSON.stringify(shoppingList.value))
-
-        await AddProductsAndCalculateList(shoppingList.value)
-
-        getCategories()
-    }
-
-    async function removeProductFromShoppingList(productId) {
-        const productIndex = productsInShoppingList.value.findIndex(product => product.productId === productId)
-
-        productsInShoppingList.value.splice(productIndex, 1)
-        shoppingList.value.splice(productIndex,1)
-        localStorage.setItem('shoppingList', JSON.stringify(shoppingList.value))
-
-        getCategories()
-        await AddProductsAndCalculateList(shoppingList.value)
-    }
-
-    function getCategories() {
-        const categoriesMap = new Map()
-
-        productsInShoppingList.value.forEach(product => {
-            categoriesMap.set(product.productCategoryId, product.productCategoryName)
-        })
-
-        categories.value = Array.from(categoriesMap.keys())
-            .sort((a, b) => a - b)
-            .map(categoryId => categoriesMap.get(categoryId));
-    }
-
-    async function AddProductsAndCalculateList(products) {
-        const {data} = await useFetch('https://localhost:7124/api/TemporaryShoppingList/AddProductAndCalculateList', {
-            method: 'POST',
-            body: JSON.stringify(products),
-        })
-        costsPerMarket.value = data
-    }
-
-    return {
-        products,
-        productsInShoppingList,
-        categories,
-        costsPerMarket,
-        shoppingList,
-        loadProducts,
-        addProductToShoppingList,
-        removeProductFromShoppingList
-    }
-})
+)
