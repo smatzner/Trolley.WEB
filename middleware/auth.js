4ec5f79fa08eb24b5156e8e57e69c7ff857c0483
Stefan Matzner <stefanmatzner@gmx.at>
2023-12-18 09:32:36 +0100

    removed console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,6 @@ export default defineNuxtRouteMiddleware(() => {
     const authStore = useAuthStore()
     const isLoggedIn = computed(() => authStore.isLoggedIn)
 
-    console.log(isLoggedIn)
     if (!isLoggedIn) {
         throw createError({ statusCode: 401, statusMessage: 'Unauthorized' })
     }
