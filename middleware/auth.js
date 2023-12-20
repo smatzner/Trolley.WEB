ead6f7a6b544f452fd54f19744a488aac265f66c
Stefan Matzner <stefanmatzner@gmx.at>
2023-12-20 12:56:56 +0100

    fixed auth store to be properly triggered when not logged in

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware(() => {
     const authStore = useAuthStore()
     const isLoggedIn = computed(() => authStore.isLoggedIn)
 
-    if (!isLoggedIn) {
+    if (!isLoggedIn.value) {
         throw createError({ statusCode: 401, statusMessage: 'Unauthorized' })
     }
 })
--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,5 +1,8 @@
 
 <script setup>
+definePageMeta({
+  middleware: 'auth'
+})
 
 </script>
 
